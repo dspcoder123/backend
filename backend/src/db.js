69f0d0972d96d22508d31876155544d98d0a4680
dspcoder123 <dspcoder123@gmail.com>
2025-10-07 17:20:16 +0530

    mongo url updated

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
-  const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/';
+  const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://dspcoder123:Dinesh%40123@cluster524.xavl3.mongodb.net/MyDatabase?retryWrites=true&w=majority&appName=Cluster524", { useNewUrlParser: true, useUnifiedTopology: true }';
   const dbName = process.env.MONGODB_DB || 'visitsdb';
   const fullUri = mongoUri.endsWith('/') ? `${mongoUri}${dbName}` : `${mongoUri}/${dbName}`;
 
